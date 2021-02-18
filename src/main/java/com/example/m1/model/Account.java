1f7311e207ce5bb547df7d318fae26210e784ca6
Tomislav Dorić <tdoric@intranet.fina.hr>
2021-02-18 18:13:27 +0100

    Default constructor

--- src/main/java/com/example/m1/model/Account.java
+++ src/main/java/com/example/m1/model/Account.java
@@ -2,11 +2,11 @@ package com.example.m1.model;
 
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
 public class Account {
 	
 	public Account(String username, String email, String encodePassword) {
